c1fe8859e0445dd6c511ca97d782b1bf2672ca7c
LOOKATmixer <65297330+LOOKATmixer@users.noreply.github.com>
2021-04-24 16:16:49 +0500

    init commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$(document).ready(function (){
+  $('.header__burger').click(function(event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('.header__phone,.header__info').removeClass('active-phone');
+    $('body').toggleClass('lock');
+  });
+});
+
+$(document).ready(function (){
+  $('.header__phone').click(function(event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+    $('.header__phone,.header__info').toggleClass('active-phone');
+    $('body').toggleClass('lock');
+  });
+});
+
+$(document).ready(function (){
+  $('.slider').slick({
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    draggable: false,
+    centerMode: true,
+    variableWidth: true,
+    rows: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          autoplay: false,
+          centerMode: false,
+          variableWidth: false,
+          rows: 2,
+        }
+      }
+    ]
+  });
+});
+
