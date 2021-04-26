2423e4c53971ea384ff56efaea6a1c8d8a390a26
LOOKATmixer <65297330+LOOKATmixer@users.noreply.github.com>
2021-04-26 22:13:25 +0500

    added block about, fixed slider

--- js/index.js
+++ js/index.js
@@ -1,43 +1,32 @@
-$(window).on('resize', function () {
-  if ($(window).width() > 767) $('.slider').remove('.slider__item-mobile');
-});
-
 $(document).ready(function () {
+  $(window).resize(function(){
+      $('.slider').slick('refresh');
+  })
+  
   $('.header__burger').click(function (event) {
     $('.header__burger,.header__menu').toggleClass('active');
-    $('.header__phone,.header__info').removeClass('active-phone');
-    $('body').toggleClass('lock');
   });
-});
-
-$(document).ready(function () {
-  $('.header__phone').click(function (event) {
-    $('.header__burger,.header__menu').toggleClass('active');
-    $('.header__phone,.header__info').toggleClass('active-phone');
-    $('body').toggleClass('lock');
-  });
-});
-
-$(document).ready(function () {
-  $('.slider').slick({
+  
+  $('.desktop-slider').slick({
     adaptiveHeight: true,
     slidesToShow: 3,
-    autoplay: true,
-    autoplaySpeed: 1500,
+    //autoplay: false,
+    //autoplaySpeed: 1500,
     draggable: false,
     centerMode: true,
     variableWidth: true,
     rows: 1,
-    responsive: [
-      {
-        breakpoint: 767,
-        settings: {
-          autoplay: false,
-          centerMode: false,
-          variableWidth: false,
-          rows: 2,
-        },
-      },
-    ],
+  });
+  
+  $('.mobile-slider').slick({
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    //autoplay: false,
+    //autoplaySpeed: 1500,
+    draggable: false,
+    autoplay: false,
+    centerMode: false,
+    variableWidth: false,
+    rows: 2,
   });
 });
